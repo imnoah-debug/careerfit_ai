4dc22c453a3776edf45d0311899011ff659d0e30
imnoah-debug <shdk1515@gmail.com>
2026-07-06 14:31:49 +0900

    main.py 수정

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,14 @@ version="0.1.0"
 # 요리 비유: 다른 건물(프론트엔드)에서 오는 배달 요청을 허용하는 설정
 app.add_middleware(
 CORSMiddleware,
-allow_origins=["http://localhost:5173"],
+allow_origins=[
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:5175",
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:5174",
+    "http://127.0.0.1:5175",
+],
 allow_credentials=True,
 allow_methods=["*"],
 allow_headers=["*"],
